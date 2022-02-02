7ca4eab5c6b153883b25cc9b7240a9c11bd50b94
Mehdi <mehdi.chamiani@gmail.com>
2022-02-03 02:35:57 +0400

    Leaflet library added

--- script.js
+++ script.js
@@ -14,10 +14,22 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const {latitude} = position.coords;
-      const {longitude} = position.coords;
-      console.log(latitude,longitude);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
 
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position from the');
